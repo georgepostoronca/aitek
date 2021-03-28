cb4ba34751ae1855e0a6afbd88a096ee55cdb2e6
George <george.postoronca@gmail.com>
2021-03-28 23:22:13 +0300

    block slider and block menu

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,32 @@ const headslid = new Swiper('.js-headslider', {
     clickable: true
   }
 });
+
+// Fixed Scroll
+const menuEl = document.querySelector(".js-menu");
+console.log(menuEl)
+document.addEventListener("scroll", function() {
+  if(window.scrollY > 10) {
+    menuEl.classList.add("--fixed")
+  } else {
+    menuEl.classList.remove("--fixed")
+  }
+});
+
+
+// Open/Close Menu
+const menuClose = document.querySelector(".js-menu-close");
+const menuOpen = document.querySelector(".js-menu-open");
+const menuElBlock = document.querySelector(".js-menu-el");
+
+if(menuElBlock) {
+  menuClose.addEventListener("click", function() {
+    menuElBlock.classList.remove("--active");
+    document.body.classList.remove("block-scroll");
+  });
+
+  menuOpen.addEventListener("click", function() {
+    menuElBlock.classList.add("--active");
+    document.body.classList.add("block-scroll");
+  })
+}
